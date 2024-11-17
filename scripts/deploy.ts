8ec1d7725d17d0f8ad0068d83b806b9c48955236
pumpxDev <tangzeyong@pumpx.io>
2024-11-17 20:01:01 +0800

    feat: 部署测试网

---
+++ scripts/deploy.ts
@@ -0,0 +1,39 @@
+import { beginCell, Cell, contractAddress, StateInit, storeStateInit, toNano } from "@ton/core"
+import {hex} from '../build/main.compiled.json'
+import qs from "qs"
+import qrcode from "qrcode-terminal";
+
+async function deployScript() {
+    const codeCell = Cell.fromBoc(Buffer.from(hex, 'hex'))[0]
+    const dataCell = new Cell()
+
+    const stateInit: StateInit = {
+        code: codeCell,
+        data: dataCell,
+    }
+
+    const stateInitBuilder = beginCell();
+    storeStateInit(stateInit)(stateInitBuilder);
+    const stateInitCell = stateInitBuilder.endCell()
+    const address = contractAddress(0, {
+        code: codeCell,
+        data: dataCell
+    })
+    console.log('address is:', address)
+    let link =
+        `https://tonhub.com/transfer/` +
+        address.toString({
+            testOnly: true,
+        }) + "?" +
+        qs.stringify({
+        text: "Deploy contract",
+        amount: toNano(1).toString(10),
+        init: stateInitCell.toBoc({ idx: false }).toString("base64"),
+        });
+
+    qrcode.generate(link, { small: true }, (code) => {
+        console.log(code);
+    });
+}
+
+deployScript()
